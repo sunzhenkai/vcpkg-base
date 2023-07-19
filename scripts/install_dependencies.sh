3fd31e73930ad8c9202d23e19c0cd48e52bc5a36
zhenkai.sun <zhenkai.sun@qq.com>
2023-07-19 20:17:58 +0800

    add install scripts

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# check os information
+if [ -f "/etc/os-release" ]; then
+    . /etc/os-release
+elif [ -f "/etc/arch-release" ]; then
+    export ID=arch
+else
+    echo "/etc/os-release missing."
+    exit 1
+fi
+
+debian_packages=(
+)
+
+fedora_packages=(
+)
+
+# centos7 & centos8 common packages
+redhat_packages=(
+)
+
+centos7_packages=(
+    "${redhat_packages[@]}"
+)
+
+centos8_packages=(
+    "${redhat_packages[@]}"
+)
+
+arch_packages=(
+    curl
+    zip
+    unzip
+    tar
+    cmake
+    ninja
+    make
+    git
+)
+
+opensuse_packages=(
+)
+
+case "$ID" in
+ubuntu | debian | pop)
+    apt-get install -y "${debian_packages[@]}"
+    ;;
+fedora)
+    dnf install -y "${fedora_packages[@]}"
+    ;;
+rhel | centos | rocky)
+    if [ "$VERSION_ID" = "7" ]; then
+        yum install -y epel-release centos-release-scl scl-utils
+        yum install -y "${centos7_packages[@]}"
+    elif [ "${VERSION_ID%%.*}" = "8" ]; then
+        dnf install -y epel-release
+        dnf install -y "${centos8_packages[@]}"
+    fi
+    ;;
+opensuse-leap)
+    zypper install -y "${opensuse_packages[@]}"
+    ;;
+arch | manjaro)
+    if [ "$EUID" -eq "0" ]; then
+        pacman -Sy --needed --noconfirm "${arch_packages[@]}"
+    else
+        echo "running without root. Skipping main dependencies (pacman)." 1>&2
+    fi
+    ;;
+*)
+    echo "Your system ($ID) is not supported by this script. Please install dependencies manually."
+    exit 1
+    ;;
+esac
---
+++ scripts/install_vcpkg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+# variables
+BASE=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+INSTALL_DIR=$HOME/.local
+
+[ ! -e "$INSTALL_DIR" ] && mkdir -p "$INSTALL_DIR" && cd "$INSTALL_DIR"
+bash "$BASE/install_dependencies.sh"
+git clone https://github.com/Microsoft/vcpkg.git
+bash "vcpkg/bootstrap-vcpkg.sh"
+
+if [ -e "vcpkg/vcpkg" ]; then
+    exit 0
+else
+    echo "install vcpkg failed."
+    exit 1
+fi
