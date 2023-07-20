e4d3c68e4c2c9b7734e3d17ce10d2c903427c40f
zhenkai.sun <zhenkai.sun@qq.com>
2023-07-20 16:20:00 +0800

    add logger

--- ports/seastar/portfile.cmake
+++ ports/seastar/portfile.cmake
@@ -1,4 +1,5 @@
-vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
         REPO scylladb/seastar
         REF seastar-22.11.0
         SHA512 134a6558ead7bf122eb2c02e1059110784764d6c4a281f4e214ea20857ae4a74e4bcd2375671687cfc350d81823b66ceb20e169c0f5aaa7a8960a1bde38085f6
@@ -6,6 +7,7 @@ vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
         PATCHES cmake_dest.patch
         )
 
+message(STATUS "source path of seastar is ${SOURCE_PATH}")
 if (VCPKG_TARGET_IS_LINUX)
     message(STATUS "Seastar currently requires the following libraries from the system package manager:\n    libpciaccess\n    xfslibs\n    libgnutls28\n    libsctp\n    systemtap-sdt\n    libtool\n    valgrind \n\nThese can be installed on Ubuntu systems via apt-get install libpciaccess-dev xfslibs-dev libgnutls28-dev libsctp-dev systemtap-sdt-dev libtool valgrind")
 endif ()
