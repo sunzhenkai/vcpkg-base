0aadc5d52c9a9f53ca92be94a2ee16b6a3d6bd22
zhenkai.sun <zhenkai.sun@qq.com>
2023-08-23 14:21:45 +0800

    update cpp-common

--- ports/cpp-common/portfile.cmake
+++ ports/cpp-common/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO sunzhenkai/cpp-common
-        REF 0.0.2
-        SHA512 63925e74bfa31321a8efef689f6f591cec9fe3542c591da53215d8fa0e0f72820a2829250715aeccbdbb1fa4d431059bfd6bd49e487b85bbdac8315ddb9b2b61
+        REF 0.0.3
+        SHA512 fa5075bdca9c2f431e2b44600abb933af33aca9d7ce75164bb05b488eaa0164db4a5034634eff0c6f56b8d28350612de70db899454c9e0a74a206cf8e2e3f560
         HEAD_REF main
 )
 
