4e28eb7806002a7e7f3dae51af79dec3be0d5e36
Wii <zhenkai.sun@qq.com>
2023-08-23 10:20:45 +0800

    fix datastax-cpp-driver (#3)
    fix datastax-cpp-driver

--- ports/datastax-cpp-driver/portfile.cmake
+++ ports/datastax-cpp-driver/portfile.cmake
@@ -4,12 +4,10 @@ vcpkg_from_github(
         REF 2.17.0
         SHA512 9589a6399f536ab03c27755e646c1195dd911c47c465815c21c15226e809cc4f8691f7c5a74ca0866b9aef377da16532be415015f544a939ea46ecd5644dff18
         HEAD_REF master
-        )
+        PATCHES cmake_deps.patch
+)
 
-message(STATUS "source path of seastar is ${SOURCE_PATH}")
-if (VCPKG_TARGET_IS_LINUX)
-    message(STATUS "Seastar currently requires the following libraries from the system package manager:\n    libpciaccess\n    xfslibs\n    libgnutls28\n    libsctp\n    systemtap-sdt\n    libtool\n    valgrind \n\nThese can be installed on Ubuntu systems via apt-get install libpciaccess-dev xfslibs-dev libgnutls28-dev libsctp-dev systemtap-sdt-dev libtool valgrind")
-endif ()
+message(STATUS "source path is ${SOURCE_PATH}")
 
 vcpkg_configure_cmake(
         SOURCE_PATH ${SOURCE_PATH}
