8b8c672e84750c6a81814bae3d8480b0fb5f13c7
Wii <zhenkai.sun@qq.com>
2024-04-22 20:09:29 +0800

    cpp common (#5)
    cpp common

--- ports/cpp-common/portfile.cmake
+++ ports/cpp-common/portfile.cmake
@@ -1,18 +1,24 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
 vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO sunzhenkai/cpp-common
-        REF 0.0.7
-        SHA512 d36a6cbbe9613d1f278a4ba2a72ad9fdb3b0af26e53e9a63cf3cc745fd6568828e70b8d748cfcebbab322dc11630d0a6a14670cdec9dd38950d32504fdfd5d17
-        HEAD_REF main
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO sunzhenkai/cpp-common
+    REF "${VERSION}"
+    SHA512 33fbc6e82cd313105278c04e840ff9af5da0db1d4072dc34492a7fb86ff73bf08e27e3822bfaf50f849fbcb34b4edde022b41891ddaf7ff5f559b75d3f62fedb
+    HEAD_REF main
 )
 
-message(STATUS "source path is ${SOURCE_PATH}, current packages dir is ${CURRENT_PACKAGES_DIR}")
 
 vcpkg_cmake_configure(
-        SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_install_cmake()
-vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "cpp-common")
 vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
