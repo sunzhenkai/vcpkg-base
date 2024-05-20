5c91f1145b355257680829fd10498d1a4d48bcd8
Wii <zhenkai.sun@qq.com>
2024-05-20 23:32:08 +0800

    Wip leveldb (#10)

---
+++ ports/leveldb/portfile.cmake
@@ -0,0 +1,38 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://github.com/google/leveldb.git
+    REF "99b3c03b3284f5886f9ef9a4ef703d57373e61be"
+    HEAD_REF master
+    PATCHES build.patch
+)
+
+file(COPY "${CURRENT_PORT_DIR}/leveldbConfig.cmake.in" DESTINATION "${SOURCE_PATH}/cmake")
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        crc32c WITH_CRC32C
+        snappy WITH_SNAPPY
+        tcmalloc WITH_TCMALLOC
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DLEVELDB_BUILD_TESTS=OFF
+        -DLEVELDB_BUILD_BENCHMARKS=OFF
+    OPTIONS_DEBUG
+        -DINSTALL_HEADERS=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
+
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
