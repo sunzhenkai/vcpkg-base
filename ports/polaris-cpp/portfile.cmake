f51d651de805746687d149b82b1a326e3fd5c19d
zhenkai.sun <zhenkai.sun@qq.com>
2024-06-05 19:51:45 +0800

    update

--- ports/polaris-cpp/portfile.cmake
+++ ports/polaris-cpp/portfile.cmake
@@ -11,7 +11,8 @@ message(STATUS "source path is ${SOURCE_PATH}, current packages dir is ${CURRENT
 
 vcpkg_execute_build_process(
     COMMAND make -j
-    WORKING_DIRECTORY ${SOURCE_PATH})
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    LOGNAME ${PORT}-build)
 
 # for debug
 #execute_process(COMMAND mkdir -p ${SOURCE_PATH}/build64/lib COMMAND touch ${SOURCE_PATH}/build64/lib/libpolaris_api.a)
