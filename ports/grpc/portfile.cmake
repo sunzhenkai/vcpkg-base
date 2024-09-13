4ed092ba1d58312bdf8a81907b6baff5c0024d8e
zhenkai.sun <zhenkai.sun@qq.com>
2024-09-13 15:11:05 +0800

    add grpc

--- ports/grpc/portfile.cmake
+++ ports/grpc/portfile.cmake
@@ -1,49 +1,72 @@
 include(vcpkg_common_functions)
 
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore" OR NOT VCPKG_CMAKE_SYSTEM_NAME)
+if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore" OR NOT VCPKG_CMAKE_SYSTEM_NAME)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
+endif ()
 
 # This snippet is a workaround for users who are upgrading from an extremely old version of this
 # port, which cloned directly into `src\`
-if(EXISTS "${CURRENT_BUILDTREES_DIR}/src/.git")
+if (EXISTS "${CURRENT_BUILDTREES_DIR}/src/.git")
     file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/src)
-endif()
+endif ()
 
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO grpc/grpc
-    REF 08fd59f039c7cf62614ab7741b3f34527af103c7
-    SHA512 a1200e0df981f69f9831425a5187dcc843d9eadee1663150cd3aa10d388ae0693ac6aa57760d7047ce1e6c55a15339d1db2f1b6e25f84c76ad97e0b48706e2c4
-    HEAD_REF master
-    PATCHES
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO grpc/grpc
+        REF v1.26.0
+        SHA512 4f7d067c582d0a9be7b7d4dd1e08acdbe6ff0686dfe92b5db5b7927f779410a4237acfd1c61d9f79eb2d00f86cf93cc7492fa372c1877e57c1aa23e9bb2648bb
+        HEAD_REF master
+        PATCHES
         00001-fix-uwp.patch
         00002-static-linking-in-linux.patch
         00003-undef-base64-macro.patch
         00004-link-gdi32-on-windows.patch
         00005-fix-uwp-error.patch
+        00006-crypt32.patch
+        00008-uwp_upb_disable_C4146_error.patch
+        00009_remove_plugins.patch
 )
 
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
     set(gRPC_BUILD_CODEGEN OFF)
-else()
+else ()
     set(gRPC_BUILD_CODEGEN ON)
-endif()
+endif ()
 
-if(VCPKG_TARGET_IS_UWP)
+if (VCPKG_CRT_LINKAGE STREQUAL "static")
+    set(gRPC_MSVC_STATIC_RUNTIME ON)
+else ()
+    set(gRPC_MSVC_STATIC_RUNTIME OFF)
+endif ()
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    set(gRPC_STATIC_LINKING ON)
+else ()
+    set(gRPC_STATIC_LINKING OFF)
+endif ()
+
+if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
     set(cares_CARES_PROVIDER OFF)
-else()
+else ()
     set(cares_CARES_PROVIDER "package")
-endif()
+endif ()
+
+# CmakeLists.txt https://github.com/grpc/grpc/blob/v1.26.0/CMakeLists.txt
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+        FEATURES
+        ruby WITH_RUBY
+        php WITH_PHP
+        objc WITH_OBJ_C
+)
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
+        SOURCE_PATH ${SOURCE_PATH}
+        PREFER_NINJA
+        OPTIONS
         -DgRPC_INSTALL=ON
         -DgRPC_BUILD_TESTS=OFF
-        -DgRPC_STATIC_LINKING=ON
-        -DgRPC_MSVC_STATIC_RUNTIME=ON
+        -DgRPC_STATIC_LINKING=${gRPC_STATIC_LINKING}
+        -DgRPC_MSVC_STATIC_RUNTIME=${gRPC_MSVC_STATIC_RUNTIME}
         -DgRPC_ZLIB_PROVIDER=package
         -DgRPC_SSL_PROVIDER=package
         -DgRPC_PROTOBUF_PROVIDER=package
@@ -57,6 +80,9 @@ vcpkg_configure_cmake(
         -DgRPC_INSTALL_INCLUDEDIR:STRING=include
         -DgRPC_INSTALL_CMAKEDIR:STRING=share/grpc
         -DgRPC_BUILD_CODEGEN=${gRPC_BUILD_CODEGEN}
+        -DgRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN=${WITH_OBJ_C}
+        -DgRPC_BUILD_GRPC_RUBY_PLUGIN=${WITH_RUBY}
+        -DgRPC_BUILD_GRPC_PHP_PLUGIN=${WITH_PHP}
 )
 
 vcpkg_install_cmake(ADD_BIN_TO_PATH)
