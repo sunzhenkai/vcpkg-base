de21ef7527c33a42ae76fc2992122b2ad9ed5ea4
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-14 11:43:23 +0000

    as client down

--- ports/aerospike-client/portfile.cmake
+++ ports/aerospike-client/portfile.cmake
@@ -4,7 +4,7 @@ set(GIT_REF 7.0.3)
 set(GIT_URL https://github.com/aerospike/aerospike-client-c.git)
 
 # custome clone git repo
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/${GIT_REF})
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${GIT_REF})
 file(MAKE_DIRECTORY ${SOURCE_PATH})
 if(NOT EXISTS "${SOURCE_PATH}/.git")
         message(STATUS "Cloning")
@@ -42,8 +42,24 @@ message(STATUS "LD_LIBRARY_PATH: $ENV{LD_LIBRARY_PATH}")
 message(STATUS "CPATH: $ENV{CPATH}")
 message(STATUS "AR: $ENV{AR}")
 message(STATUS "CMAKE_AR: $ENV{CMAKE_AR}")
-vcpkg_configure_make(SOURCE_PATH "${SOURCE_PATH}" COPY_SOURCE SKIP_CONFIGURE)
-vcpkg_build_make(DISABLE_PARALLEL ENABLE_INSTALL)
+message(STATUS "CURRENT_PACKAGES_DIR: ${CURRENT_PACKAGES_DIR}")
+message(STATUS "INSTALL_PREFIX: ${INSTALL_PREFIX}")
+message(STATUS "CMAKE_INSTALL_PREFIX: ${CMAKE_INSTALL_PREFIX}")
+message(STATUS "CURRENT_INSTALLED_DIR: ${CURRENT_INSTALLED_DIR}")
+
+# install prefix fixup: 由于 aerospike client 没有 configure 脚本，所以需要手动设置安装路径 INSTALL_PREFIX
+# INSTALL_PREFIX 是代码文件 pkg/install 内定义的
+# 这里的 INSTALL_PREFIX 是 {prefix_dir}/{install_dir}
+# vcpkg_configure_make 默认会指定 --prefix, 但是这里没有 configure 脚本，添加了 SKIP_CONFIGURE 选项
+# 因此这里修复 INSTALL_PREFIX 为 {prefix_dir}/{install_dir}
+# 不然这里会报错: https://github.com/microsoft/vcpkg/blob/master/scripts/cmake/vcpkg_build_make.cmake#L177C8-L177C102
+# 报错代码： file(RENAME "${CURRENT_PACKAGES_DIR}_tmp${Z_VCPKG_INSTALL_PREFIX}" "${CURRENT_PACKAGES_DIR}") 
+string(REGEX REPLACE "([a-zA-Z]):/" "/\\1/" Z_VCPKG_INSTALL_PREFIX "${CURRENT_INSTALLED_DIR}")
+set(ENV{INSTALL_PREFIX} "${CURRENT_PACKAGES_DIR}${Z_VCPKG_INSTALL_PREFIX}")
+
+vcpkg_configure_make(SOURCE_PATH "${SOURCE_PATH}" COPY_SOURCE SKIP_CONFIGURE OPTIONS "CURRENT_INSTALLED_DIR=${CURRENT_INSTALLED_DIR}")
+vcpkg_build_make()
+vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
