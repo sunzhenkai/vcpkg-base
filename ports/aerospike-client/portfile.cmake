5bd3811ebd3b355067d272bb97b5fc8789bd775e
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-14 07:22:52 +0000

    as client donw

--- ports/aerospike-client/portfile.cmake
+++ ports/aerospike-client/portfile.cmake
@@ -6,14 +6,14 @@ set(GIT_URL https://github.com/aerospike/aerospike-client-c.git)
 # custome clone git repo
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/${GIT_REF})
 file(MAKE_DIRECTORY ${SOURCE_PATH})
-if (NOT EXISTS "${SOURCE_PATH}/.git")
-    message(STATUS "Cloning")
-    vcpkg_execute_required_process(
-            COMMAND ${GIT} clone ${GIT_URL} ${SOURCE_PATH}
-            WORKING_DIRECTORY ${SOURCE_PATH}
-            LOGNAME clone
-    )
-endif ()
+if(NOT EXISTS "${SOURCE_PATH}/.git")
+        message(STATUS "Cloning")
+        vcpkg_execute_required_process(
+                COMMAND ${GIT} clone ${GIT_URL} ${SOURCE_PATH}
+                WORKING_DIRECTORY ${SOURCE_PATH}
+                LOGNAME clone
+        )
+endif()
 
 message(STATUS "Checkout revision ${GIT_REF}")
 vcpkg_execute_required_process(
@@ -29,22 +29,16 @@ vcpkg_execute_required_process(
         LOGNAME submodule
 )
 
-set(MAKE_OPTS "")
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    list(APPEND MAKE_OPTS "STATIC=1")
-else()
-    list(APPEND MAKE_OPTS "SHARED=1")
-endif()
-#vcpkg_configure_make(
-#    SOURCE_PATH ${SOURCE_PATH}
-#    PROJECT_SUBPATH .  # 如果项目的 Makefile 不在根目录，可以指定子路径
-#    OPTIONS ${MAKE_OPTS}
-#)
+vcpkg_execute_required_process(
+        COMMAND bash -c "echo $PATH && echo $LD_LIBRARY_PATH"
+        WORKING_DIRECTORY ${SOURCE_PATH}
+        LOGNAME verbose)
 
-vcpkg_install_make()
-#vcpkg_fixup_pkgconfig()
+vcpkg_configure_make(SOURCE_PATH "${SOURCE_PATH}" COPY_SOURCE SKIP_CONFIGURE)
+vcpkg_build_make(OPTIONS STATIC=1 SHARED=0 DISABLE_PARALLEL)
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
