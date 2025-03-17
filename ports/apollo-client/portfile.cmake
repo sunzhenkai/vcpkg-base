31d740260f908234f603ce41466025f076b78371
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-17 17:32:37 +0800

    update

--- ports/apollo-client/portfile.cmake
+++ ports/apollo-client/portfile.cmake
@@ -1,20 +1,28 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_git(
-        OUT_SOURCE_PATH SOURCE_PATH
-        URL https://github.com/lzeqian/apollo.git
-        REF "734ae75cb35043c816aa5ed70c66822886c585fa"
-        HEAD_REF master
-        PATCHES cmake.patch)
+  OUT_SOURCE_PATH
+  SOURCE_PATH
+  URL
+  https://github.com/sunzhenkai/apollo-client-cpp.git
+  REF
+  11c38050f947de88434129139b2355c24579593f
+  HEAD_REF
+  master)
 
-message(STATUS "source path is ${SOURCE_PATH}, current packages dir is ${CURRENT_PACKAGES_DIR}")
+message(
+  STATUS
+    "source path is ${SOURCE_PATH}, current packages dir is ${CURRENT_PACKAGES_DIR}"
+)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_install_cmake()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/apollo-client/cygwin")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-#file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
