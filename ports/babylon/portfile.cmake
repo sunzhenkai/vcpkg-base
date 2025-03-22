1fe86ddbc2dee80be0cb86acd929fce0f54f9bb9
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-22 16:19:33 +0800

    update

--- ports/babylon/portfile.cmake
+++ ports/babylon/portfile.cmake
@@ -1,13 +1,19 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_git(
-        OUT_SOURCE_PATH SOURCE_PATH
-        URL https://github.com/baidu/babylon.git
-        REF "1abcbd34df84f69fe3d1f26ae79fa7a2398083b4"
-        HEAD_REF master
-)
+  OUT_SOURCE_PATH
+  SOURCE_PATH
+  URL
+  https://github.com/baidu/babylon.git
+  REF
+  "571cfb36eaa51eb1b6f1d5b63c515011f979f1bc"
+  HEAD_REF
+  master)
 
-message(STATUS "source path is ${SOURCE_PATH}, current packages dir is ${CURRENT_PACKAGES_DIR}")
+message(
+  STATUS
+    "source path is ${SOURCE_PATH}, current packages dir is ${CURRENT_PACKAGES_DIR}"
+)
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_install_cmake()
@@ -16,5 +22,9 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME "${PORT}" CONFIG_PATH "lib/cmake/babylon")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/babylon/reusable/patch")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(
+  INSTALL ${SOURCE_PATH}/LICENSE
+  DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+  RENAME copyright)
