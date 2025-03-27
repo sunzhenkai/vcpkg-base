fa89a03be370e2329a65a4d2205d065fd655d3e4
zhenkai.sun <zhenkai.sun@qq.com>
2025-03-27 15:21:22 +0800

    update

--- cmakes/protobuf.cmake
+++ cmakes/protobuf.cmake
@@ -1,53 +1,115 @@
-FUNCTION(GenerateProtoBufMessage)
-    CMAKE_PARSE_ARGUMENTS(ARG "" "SRCS;HEADERS;OUTPUT;IMPORT" "FILES;EXTERN_IMPORT" ${ARGN})
-    # find protoc
-    IF (NOT TARGET protobuf::protoc)
-        FIND_PACKAGE(Protobuf CONFIG REQUIRED)
-        IF (NOT TARGET protobuf::protoc)
-            MESSAGE(FATAL_ERROR "Target protobuf::protoc not found")
-        ENDIF ()
-    ENDIF ()
-    FILE(MAKE_DIRECTORY ${ARG_OUTPUT})
-    FILE(GLOB PROTO_FILES ${ARG_FILES})
-    GET_PROPERTY(PB_EXECUTABLE TARGET protobuf::protoc PROPERTY LOCATION)
+macro(FIND_BIN_PROTOC)
+  # find protoc
+  if(NOT TARGET protobuf::protoc)
+    find_package(Protobuf CONFIG REQUIRED)
+    if(NOT TARGET protobuf::protoc)
+      message(FATAL_ERROR "Target protobuf::protoc not found")
+    endif()
+  endif()
+  file(MAKE_DIRECTORY ${ARG_OUTPUT})
+  file(GLOB PROTO_FILES ${ARG_FILES})
+  get_property(
+    PB_EXECUTABLE
+    TARGET protobuf::protoc
+    PROPERTY LOCATION)
+endmacro(FIND_BIN_PROTOC)
 
-    STRING(LENGTH ${ARG_IMPORT} PROTO_PREFIX_LENGTH)
-    SET(IMPORT_ARGS "-I${ARG_IMPORT}")
-    FOREACH (I IN LISTS ARG_EXTERN_IMPORT)
-        SET(IMPORT_ARGS "-I${I} ${IMPORT_ARGS}")
-    ENDFOREACH ()
-    FOREACH (I ${PROTO_FILES})
-        STRING(LENGTH ${I} I_PATH_LENGTH)
-        STRING(SUBSTRING ${I} 0 ${PROTO_PREFIX_LENGTH} I_PREFIX)
-        IF (NOT I_PREFIX STREQUAL ARG_IMPORT)
-            MESSAGE(FATAL_ERROR "[GenerateProtoBufMessage] proto file not with import path. "
-                    "[proto=${I_PREFIX}, import_path=${ARG_IMPORT}]")
-        ENDIF ()
-        STRING(SUBSTRING ${I} ${PROTO_PREFIX_LENGTH} ${I_PATH_LENGTH} REV_PATH)
-        StringStartsWith(${REV_PATH} "/" REV_PATH_SLASH)
-        IF (REV_PATH_SLASH)
-            SET(PROTO_PREFIX_LENGTH_TMP ${PROTO_PREFIX_LENGTH})
-            MATH(EXPR PROTO_PREFIX_LENGTH_TMP "${PROTO_PREFIX_LENGTH_TMP}+1")
-            STRING(SUBSTRING ${I} ${PROTO_PREFIX_LENGTH_TMP} ${I_PATH_LENGTH} REV_PATH)
-        ENDIF ()
-        RemoveExtension(${REV_PATH} REV_PATH)
-        EXECUTE_PROCESS(
-                COMMAND bash -c "${PB_EXECUTABLE} ${IMPORT_ARGS} --cpp_out ${ARG_OUTPUT} ${I}"
-                RESULT_VARIABLE rc
-        )
-        if (NOT "${rc}" STREQUAL "0")
-            message(FATAL_ERROR "[GenerateProtoBufMessage] generate ${I} cpp code failed. [message=${rc}]")
-        else ()
-            LIST(APPEND ${ARG_SRCS} "${ARG_OUTPUT}/${REV_PATH}.pb.cc")
-            # LIST(APPEND ${ARG_HEADERS} "${ARG_OUTPUT}/${REV_PATH}.pb.h")
-        endif ()
-    ENDFOREACH ()
+function(GenerateProtoBufMessage)
+  cmake_parse_arguments(ARG "" "SRCS;HEADERS;OUTPUT;IMPORT"
+                        "FILES;EXTERN_IMPORT" ${ARGN})
+  find_bin_protoc()
+  string(LENGTH ${ARG_IMPORT} PROTO_PREFIX_LENGTH)
+  set(IMPORT_ARGS "-I${ARG_IMPORT}")
+  foreach(I IN LISTS ARG_EXTERN_IMPORT)
+    set(IMPORT_ARGS "-I${I} ${IMPORT_ARGS}")
+  endforeach()
+  foreach(I ${PROTO_FILES})
+    string(LENGTH ${I} I_PATH_LENGTH)
+    string(SUBSTRING ${I} 0 ${PROTO_PREFIX_LENGTH} I_PREFIX)
+    if(NOT I_PREFIX STREQUAL ARG_IMPORT)
+      message(
+        FATAL_ERROR
+          "[GenerateProtoBufMessage] proto file not with import path. "
+          "[proto=${I_PREFIX}, import_path=${ARG_IMPORT}]")
+    endif()
+    string(SUBSTRING ${I} ${PROTO_PREFIX_LENGTH} ${I_PATH_LENGTH} REV_PATH)
+    string_starts_with(${REV_PATH} "/" REV_PATH_SLASH)
+    if(REV_PATH_SLASH)
+      set(PROTO_PREFIX_LENGTH_TMP ${PROTO_PREFIX_LENGTH})
+      math(EXPR PROTO_PREFIX_LENGTH_TMP "${PROTO_PREFIX_LENGTH_TMP}+1")
+      string(SUBSTRING ${I} ${PROTO_PREFIX_LENGTH_TMP} ${I_PATH_LENGTH}
+                       REV_PATH)
+    endif()
+    remove_extension(${REV_PATH} REV_PATH)
+    execute_process(
+      COMMAND bash -c
+              "${PB_EXECUTABLE} ${IMPORT_ARGS} --cpp_out ${ARG_OUTPUT} ${I}"
+      RESULT_VARIABLE rc)
+    if(NOT "${rc}" STREQUAL "0")
+      message(
+        FATAL_ERROR
+          "[GenerateProtoBufMessage] generate ${I} cpp code failed. [message=${rc}]"
+      )
+    else()
+      list(APPEND ${ARG_SRCS} "${ARG_OUTPUT}/${REV_PATH}.pb.cc")
+      # LIST(APPEND ${ARG_HEADERS} "${ARG_OUTPUT}/${REV_PATH}.pb.h")
+    endif()
+  endforeach()
 
-    # append header include directories
-    LIST(FIND ARG_HEADERS ${ARG_OUTPUT} HEADER_DIR_EXISTS)
-    IF (HEADER_DIR_EXISTS EQUAL -1)
-        LIST(APPEND ${ARG_HEADERS} "${ARG_OUTPUT}")
-        SET(${ARG_HEADERS} ${${ARG_HEADERS}} PARENT_SCOPE)
-    ENDIF ()
-    SET(${ARG_SRCS} ${${ARG_SRCS}} PARENT_SCOPE)
-ENDFUNCTION(GenerateProtoBufMessage)
+  # append header include directories
+  list(FIND ARG_HEADERS ${ARG_OUTPUT} HEADER_DIR_EXISTS)
+  if(HEADER_DIR_EXISTS EQUAL -1)
+    list(APPEND ${ARG_HEADERS} "${ARG_OUTPUT}")
+    set(${ARG_HEADERS}
+        ${${ARG_HEADERS}}
+        PARENT_SCOPE)
+  endif()
+  set(${ARG_SRCS}
+      ${${ARG_SRCS}}
+      PARENT_SCOPE)
+endfunction(GenerateProtoBufMessage)
+
+function(generate_protobuf_message_go)
+  cmake_parse_arguments(ARG "" "OUTPUT;IMPORT" "FILES;EXTERN_IMPORT" ${ARGN})
+  find_bin_protoc()
+  message(status "generate protobuf go code. [files=${arg_files}]")
+
+  string(length ${arg_import} proto_prefix_length)
+  set(import_args "-i${arg_import}")
+  foreach(i in lists arg_extern_import)
+    set(import_args "-i${i} ${import_args}")
+  endforeach()
+  foreach(i ${proto_files})
+    string(length ${i} i_path_length)
+    string(substring ${i} 0 ${proto_prefix_length} i_prefix)
+    if(not i_prefix strequal arg_import)
+      message(
+        fatal_error
+          "[generateprotobufmessage] proto file not with import path. "
+          "[proto=${i_prefix}, import_path=${arg_import}]")
+    endif()
+    string(substring ${i} ${proto_prefix_length} ${i_path_length} rev_path)
+    string_starts_with(${rev_path} "/" rev_path_slash)
+    if(rev_path_slash)
+      set(proto_prefix_length_tmp ${proto_prefix_length})
+      math(expr proto_prefix_length_tmp "${proto_prefix_length_tmp}+1")
+      string(substring ${i} ${proto_prefix_length_tmp} ${i_path_length}
+                       rev_path)
+    endif()
+    remove_extension(${rev_path} rev_path)
+    message(
+      status
+        "run command: ${pb_executable} ${import_args} --go_out ${arg_output} ${i}"
+    )
+    execute_process(
+      command bash -c
+              "${pb_executable} ${import_args} --go_out ${arg_output} ${i}"
+      result_variable rc)
+    if(not "${rc}" strequal "0")
+      message(
+        fatal_error
+          "[generateprotobufmessage] generate ${i} go code failed. [message=${rc}]"
+      )
+    endif()
+  endforeach()
+endfunction(generate_protobuf_message_go)
--- cmakes/tools.cmake
+++ cmakes/tools.cmake
@@ -1,78 +1,99 @@
-FUNCTION(RemoveExtension PT RESULT)
-    # cmake version 3.20+ required
-    CMAKE_PATH(REMOVE_EXTENSION PT LAST_ONLY)
-    SET(${RESULT} ${PT} PARENT_SCOPE)
-ENDFUNCTION(RemoveExtension)
+# internal
+function(remove_extension PT RESULT)
+  # cmake version 3.20+ required
+  cmake_path(REMOVE_EXTENSION PT LAST_ONLY)
+  set(${RESULT}
+      ${PT}
+      PARENT_SCOPE)
+endfunction(remove_extension)
 
-FUNCTION(StringStartsWith STR SEARCH RESULT)
-    STRING(FIND "${STR}" "${SEARCH}" out)
-    IF ("${out}" EQUAL 0)
-        SET(${RESULT} TRUE PARENT_SCOPE)
-    ELSE ()
-        SET(${RESULT} FALSE PARENT_SCOPE)
-    ENDIF ()
-ENDFUNCTION(StringStartsWith)
+function(string_starts_with STR SEARCH RESULT)
+  string(FIND "${STR}" "${SEARCH}" out)
+  if("${out}" EQUAL 0)
+    set(${RESULT}
+        TRUE
+        PARENT_SCOPE)
+  else()
+    set(${RESULT}
+        FALSE
+        PARENT_SCOPE)
+  endif()
+endfunction(string_starts_with)
 
-FUNCTION(PrintTargetProperties _tgt)
-    IF (NOT CMAKE_PROPERTY_LIST)
-        EXECUTE_PROCESS(COMMAND cmake --help-property-list OUTPUT_VARIABLE CMAKE_PROPERTY_LIST)
+# public
+function(PrintTargetProperties _tgt)
+  if(NOT CMAKE_PROPERTY_LIST)
+    execute_process(COMMAND cmake --help-property-list
+                    OUTPUT_VARIABLE CMAKE_PROPERTY_LIST)
 
-        # Convert command output into a CMake list
-        STRING(REGEX REPLACE ";" "\\\\;" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
-        STRING(REGEX REPLACE "\n" ";" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
-        LIST(REMOVE_DUPLICATES CMAKE_PROPERTY_LIST)
-    ENDIF ()
+    # Convert command output into a CMake list
+    string(REGEX REPLACE ";" "\\\\;" CMAKE_PROPERTY_LIST
+                         "${CMAKE_PROPERTY_LIST}")
+    string(REGEX REPLACE "\n" ";" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
+    list(REMOVE_DUPLICATES CMAKE_PROPERTY_LIST)
+  endif()
 
-    IF (NOT TARGET ${_tgt})
-        MESSAGE(STATUS "[TargetProperties] There is no target named '${_tgt}'")
-        RETURN()
-    ENDIF ()
+  if(NOT TARGET ${_tgt})
+    message(STATUS "[TargetProperties] There is no target named '${_tgt}'")
+    return()
+  endif()
 
-    FOREACH (_property ${CMAKE_PROPERTY_LIST})
-        STRING(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" property ${_property})
+  foreach(_property ${CMAKE_PROPERTY_LIST})
+    string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" property ${_property})
 
-        # Fix https://stackoverflow.com/questions/32197663/how-can-i-remove-the-the-location-property-may-not-be-read-from-target-error-i
-        IF (_property STREQUAL "LOCATION" OR _property MATCHES "^LOCATION_" OR _property MATCHES "_LOCATION$")
-            CONTINUE()
-        ENDIF ()
+    # Fix
+    # https://stackoverflow.com/questions/32197663/how-can-i-remove-the-the-location-property-may-not-be-read-from-target-error-i
+    if(_property STREQUAL "LOCATION"
+       OR _property MATCHES "^LOCATION_"
+       OR _property MATCHES "_LOCATION$")
+      continue()
+    endif()
 
-        GET_PROPERTY(_was_set TARGET ${_tgt} PROPERTY ${_property} SET)
-        IF (_was_set)
-            GET_TARGET_PROPERTY(_value ${_tgt} ${_property})
-            IF (NOT _value STREQUAL "")
-                MESSAGE("[TargetProperties] ${_tgt} ${_property} = ${_value}")
-            ENDIF ()
-        ENDIF ()
-    ENDFOREACH ()
-ENDFUNCTION(PrintTargetProperties)
+    get_property(
+      _was_set
+      TARGET ${_tgt}
+      PROPERTY ${_property}
+      SET)
+    if(_was_set)
+      get_target_property(_value ${_tgt} ${_property})
+      if(NOT _value STREQUAL "")
+        message("[TargetProperties] ${_tgt} ${_property} = ${_value}")
+      endif()
+    endif()
+  endforeach()
+endfunction(PrintTargetProperties)
 
-MACRO(AddInstall)
-    INCLUDE(GNUInstallDirs)
-    cmake_parse_arguments(ARG "" "PROJECT;EXPORT_TARGET;INCLUDE_DIR;INCLUDE_INSTALL_DIR" "TARGETS" ${ARGN})
-    IF (NOT DEFINED ARG_PROJECT)
-        MESSAGE(FATAL_ERROR "[AddInstall] PROJECT must be set")
-    ENDIF ()
+macro(AddInstall)
+  include(GNUInstallDirs)
+  cmake_parse_arguments(
+    ARG "" "PROJECT;EXPORT_TARGET;INCLUDE_DIR;INCLUDE_INSTALL_DIR" "TARGETS"
+    ${ARGN})
+  if(NOT DEFINED ARG_PROJECT)
+    message(FATAL_ERROR "[AddInstall] PROJECT must be set")
+  endif()
 
-    IF (NOT DEFINED ARG_INCLUDE_DIR)
-        SET(ARG_INCLUDE_DIR include/)
-    ENDIF ()
-    IF (NOT DEFINED ARG_INCLUDE_INSTALL_DIR)
-        SET(ARG_INCLUDE_INSTALL_DIR ${ARG_INCLUDE_DIR})
-    ENDIF ()
-    IF (NOT DEFINED ARG_EXPORT_TARGET)
-        SET(ARG_EXPORT_TARGET ${ARG_PROJECT})
-    ENDIF ()
-    IF (NOT DEFINED ARG_TARGETS)
-        SET(ARG_TARGETS ${ARG_EXPORT_TARGET})
-    ENDIF ()
-    INSTALL(TARGETS ${ARG_TARGETS}
-            EXPORT ${ARG_EXPORT_TARGET}
-            LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-            ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
-            RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
-    INSTALL(DIRECTORY ${ARG_INCLUDE_DIR} DESTINATION ${ARG_INCLUDE_INSTALL_DIR})
-    install(EXPORT ${ARG_EXPORT_TARGET}
-            DESTINATION share/${ARG_PROJECT}
-            FILE ${ARG_EXPORT_TARGET}Config.cmake
-            NAMESPACE ${ARG_PROJECT}::)
-ENDMACRO()
+  if(NOT DEFINED ARG_INCLUDE_DIR)
+    set(ARG_INCLUDE_DIR include/)
+  endif()
+  if(NOT DEFINED ARG_INCLUDE_INSTALL_DIR)
+    set(ARG_INCLUDE_INSTALL_DIR ${ARG_INCLUDE_DIR})
+  endif()
+  if(NOT DEFINED ARG_EXPORT_TARGET)
+    set(ARG_EXPORT_TARGET ${ARG_PROJECT})
+  endif()
+  if(NOT DEFINED ARG_TARGETS)
+    set(ARG_TARGETS ${ARG_EXPORT_TARGET})
+  endif()
+  install(
+    TARGETS ${ARG_TARGETS}
+    EXPORT ${ARG_EXPORT_TARGET}
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
+  install(DIRECTORY ${ARG_INCLUDE_DIR} DESTINATION ${ARG_INCLUDE_INSTALL_DIR})
+  install(
+    EXPORT ${ARG_EXPORT_TARGET}
+    DESTINATION share/${ARG_PROJECT}
+    FILE ${ARG_EXPORT_TARGET}Config.cmake
+    NAMESPACE ${ARG_PROJECT}::)
+endmacro()
