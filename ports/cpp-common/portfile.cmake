e3b7e8f980bb5654d2a33e370e90538094a39e66
zhenkai.sun <zhenkai.sun@qq.com>
2025-04-02 10:48:45 +0800

    update

--- ports/cpp-common/portfile.cmake
+++ ports/cpp-common/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH URL https://github.com/sunzhenkai/cpp-common.git
-  REF 442c5b4786a8f0d991d4aaab6786f899ef5089ba)
+  REF faebade0ad76a5120f2d6574a35ca8b2f9c9ec96)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
