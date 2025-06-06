933b27a075cb2734d2cc447e1a34163dacd11570
zhenkai.sun <zhenkai.sun@qq.com>
2025-06-06 17:33:36 +0800

    update

--- ports/kfpanda-cpp-sdk/portfile.cmake
+++ ports/kfpanda-cpp-sdk/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH URL
   https://github.com/sunzhenkai/kfpanda-cpp-sdk.git REF
-  ac5ae2a258417fda4e3dbe4dbedeaedda5218e35)
+  c87ee05c8b6a5aa0c1fc4bde3848d29687c0f709)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
