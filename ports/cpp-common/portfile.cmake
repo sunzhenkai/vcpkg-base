bf34dc7ac2c51b741ffc7061a53af84f54bfae9b
zhenkai.sun <zhenkai.sun@qq.com>
2025-06-16 07:13:13 +0800

    update

--- ports/cpp-common/portfile.cmake
+++ ports/cpp-common/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH URL https://github.com/sunzhenkai/cpp-common.git
-  REF d1a4c8b6c42bd24f16e3c3126c394c88db64fdf7)
+  REF 739304bd95be7c66187a15a92319959cb50a5f69)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS FEATURES objectstorage
                      WITH_OBJECT_STORAGE)
