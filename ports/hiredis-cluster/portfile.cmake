7b090c4e247140103b654dc488e25f6978423a50
zhenkai.sun <zhenkai.sun@qq.com>
2025-10-27 12:05:02 +0800

    add hiredis cluster

---
+++ ports/hiredis-cluster/portfile.cmake
@@ -0,0 +1,20 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_git(
+  OUT_SOURCE_PATH SOURCE_PATH URL https://github.com/Nordix/hiredis-cluster.git
+  REF d92edb8ac58d1279f5015715947f7ade61daca72)
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DENABLE_SSL=ON
+                      -DDOWNLOAD_HIREDIS=OFF -DDISABLE_TESTS=ON)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "hiredis_cluster")
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(
+  INSTALL "${SOURCE_PATH}/COPYING"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage"
+               "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
